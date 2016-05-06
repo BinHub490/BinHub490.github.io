f63abbcadceb2b7ebf5ba7762ab9969c01c03dd4
Sirfame Lin <sifame@hotmail.com>
2016-05-05 23:41:28 -0700

    Successful switch over to new DB, lots of test code that needs replacing

--- js/app.js
+++ js/app.js
@@ -6,7 +6,7 @@ angular.module('ChirperApp', ['firebase'])
 .controller('ChirperCtrl', ['$scope', '$firebaseObject', '$firebaseArray', '$firebaseAuth', function($scope, $firebaseObject, $firebaseArray, $firebaseAuth) {
 
 	/* define reference to your firebase app */
-	var ref = new Firebase("https://sirfame-isabelle-app.firebaseio.com");
+	var ref = new Firebase("https://binhub.firebaseio.com");
 
 	/* define reference to the "chirps" value in the app */
 	var chirpsRef = ref.child("chirps");
@@ -16,28 +16,22 @@ angular.module('ChirperApp', ['firebase'])
 
 	/* create a $firebaseArray for the chirps reference and add to scope */
 	$scope.chirps = $firebaseArray(chirpsRef);
-	var arr = $firebaseArray(chirpsRef);
-	console.log(typeof arr);
-	console.log($firebaseArray(chirpsRef));
-	var temp = arr.$getRecord("-K3LtNMxEPHgMdWTAx8a");
-	console.log($firebaseArray(chirpsRef).length)
 
-	arr.$loaded().then(function() {
-        console.log("loaded record:", arr.$id);
-       	// To iterate the key/value pairs of the object, use angular.forEach()
-       	angular.forEach(arr, function(value, key) {
-			names.push(value.$id);
-			console.log(key, value.$id);
-			console.log(names.length);
-    	});
-    });
-
-	/* create a $firebaseObject for the users reference and add to scope (as $scope.users) */
-	$scope.users = $firebaseObject(usersRef);
-
-	var Auth = $firebaseAuth(ref);
-
-	$scope.newUser = {};
+	// var arr = $firebaseArray(chirpsRef);
+	// console.log(typeof arr);
+	// console.log($firebaseArray(chirpsRef));
+	// var temp = arr.$getRecord("-K3LtNMxEPHgMdWTAx8a");
+	// console.log($firebaseArray(chirpsRef).length)
+	//
+	// arr.$loaded().then(function() {
+    //     console.log("loaded record:", arr.$id);
+    //    	// To iterate the key/value pairs of the object, use angular.forEach()
+    //    	angular.forEach(arr, function(value, key) {
+	// 		names.push(value.$id);
+	// 		console.log(key, value.$id);
+	// 		console.log(names.length);
+    // 	});
+    // });
 
 	/* Write an accessible (on scope) chirp() function to save a tweet */
 
@@ -49,77 +43,16 @@ angular.module('ChirperApp', ['firebase'])
 	         time:Firebase.ServerValue.TIMESTAMP
 	              // this tells firebase server to save the current time
 	   */
-	$scope.chirp = function() {
+	$scope.addBin = function() {
 		$scope.chirps.$add({
-			text: $scope.newChirp,
-			userID: -1,
+			organization: "testOrganization",
+			type: "testType",
+			location: $scope.newLocation,
 			likes: 0,
-			time: Firebase.ServerValue.TIMESTAMP
+			comment: "testComment"
 		}).then(function() {
-			$scope.newChirp= '';
+			$scope.newLocation= '';
 		});
-	}
-
-	$scope.signUp = function() {
-  	console.log("creating user " + $scope.newUser.email);
-  	//pass in an object with the new 'email' and 'password'
-  	Auth.$createUser({
-    	'email': $scope.newUser.email,
-    	'password': $scope.newUser.password
-  	})
-		.then($scope.signIn)
-		.then(function() {
-			//do stuff after signing in
-			/* values in JSON (and Firebae) cannot be undefined.
-     	if newUser.avatar is undefined, assign "img/no-pic.png" to it */
-					if(newUser.avatar === undefined) {
-						newUser.avatar = "img/no-pic.img";
-					}
-		  /* create an object with the 'handle' and 'avatar' of the newUser
-		     assign this object to the value in the $scope.users object
-		     with key of authData.uid (the user id of whoever is logged in) */
-				var newUserInfo = {
-				 'handle' : $scope.newUser.handle,
-				 'avatar' : $scope.newUser.avatar
-				};
-				$scope.users[authData.uid] = newUserInfo;
-		  /* call .$save() on the $scope.users object to save to the cloud */
-				$scope.users.$save();
-		  /* assign authData.uid to $scope.userId for our views to see */
-				$scope.userId = authData.uid;
-		})
-  	.catch(function(error){
-    	//error handling (called on the promise)
-    	console.log(error);
-  	})};
-
-	$scope.signIn = function() {
-		var promise = Auth.$authWithPassword({
-	    'email': $scope.newUser.email,
-	    'password': $scope.newUser.password
-		});
-		return promise;
-	};
-
-		//Make LogOut function available to views
-	$scope.logOut = function() {
-	   Auth.$unauth(); //"unauthorize" to log out
-	};
-
-	//Any time auth status updates, set the userId so we know
-	Auth.$onAuth(function(authData) {
-	   if(authData) { //if we are authorized
-	      $scope.userId = authData.uid;
-	   }
-	   else {
-	      $scope.userId = undefined;
-	   }
-	});
-
-	//Test if already logged in (when page load)
-	var authData = Auth.$getAuth(); //get if we're authorized
-	if(authData) {
-	   $scope.userId = authData.uid;
 	}
 
 }])
@@ -157,7 +90,7 @@ var btn = document.getElementById("myBtn");
 // Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
 
-// When the user clicks on the button, open the modal 
+// When the user clicks on the button, open the modal
 btn.onclick = function() {
     modal.style.display = "block";
 }
