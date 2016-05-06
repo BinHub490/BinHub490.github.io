763adc1149820d836249ed8ed419a7f48c7c79a6
Sirfame Lin <sifame@hotmail.com>
2016-05-06 11:56:35 -0700

    Test map

--- js/app.js
+++ js/app.js
@@ -57,27 +57,59 @@ angular.module('ChirperApp', ['firebase'])
 
 }])
 
-var map;
+// var map;
+// function initMap() {
+//   //map = new google.maps.Map(document.getElementById('map'), {
+// //	center: {lat: 47.655601, lng: -122.308903},
+// //	zoom: 15,
+// //	mapTypeId: google.maps.MapTypeId.ROADMAP
+//   //});
+//
+//   var myLatLng = {lat: 47.655601, lng: -122.308903};
+//
+//   var map = new google.maps.Map(document.getElementById('map'), {
+// 	zoom: 15,
+// 	center: myLatLng
+//   });
+//
+//   var marker = new google.maps.Marker({
+// 	position: myLatLng,
+// 	map: map,
+// 	draggable:true,
+// 	title: 'Hello World!'
+//   });
+// }
+
+var locations = [
+     ['Title A', 3.180967,101.715546, 1],
+     ['Title B', 3.200848,101.616669, 2],
+     ['Title C', 3.147372,101.597443, 3],
+     ['Title D', 3.19125,101.710052, 4]
+];
 function initMap() {
-  //map = new google.maps.Map(document.getElementById('map'), {
-//	center: {lat: 47.655601, lng: -122.308903},
-//	zoom: 15,
-//	mapTypeId: google.maps.MapTypeId.ROADMAP
-  //});
+	var map = new google.maps.Map(document.getElementById('map'), {
+	     zoom: 12,
+	     center: new google.maps.LatLng(3.171368,101.653404),
+	     mapTypeId: google.maps.MapTypeId.ROADMAP
+	});
 
-  var myLatLng = {lat: 47.655601, lng: -122.308903};
+	var infowindow = new google.maps.InfoWindow;
 
-  var map = new google.maps.Map(document.getElementById('map'), {
-	zoom: 15,
-	center: myLatLng
-  });
+	var marker, i;
 
-  var marker = new google.maps.Marker({
-	position: myLatLng,
-	map: map,
-	draggable:true,
-	title: 'Hello World!'
-  });
+	for (i = 0; i < locations.length; i++) {
+	    marker = new google.maps.Marker({
+	         position: new google.maps.LatLng(locations[i][1], locations[i][2]),
+	         map: map
+	    });
+
+	    google.maps.event.addListener(marker, 'click', (function(marker, i) {
+	         return function() {
+	             infowindow.setContent(locations[i][0]);
+	             infowindow.open(map, marker);
+	         }
+	    })(marker, i));
+	}
 }
 
 
