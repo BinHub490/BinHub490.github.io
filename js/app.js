18d320ec570285459945e67b0b57126264287985
jaimelenn <jaimelenn.uemura@gmail.com>
2016-05-06 11:54:25 -0700

    form changes and added emails

--- js/app.js
+++ js/app.js
@@ -86,6 +86,7 @@ var modal = document.getElementById('myModal');
 
 // Get the button that opens the modal
 var btn = document.getElementById("myBtn");
+var submitbtn = document.getElementById("submit")
 
 // Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
@@ -95,10 +96,16 @@ btn.onclick = function() {
     modal.style.display = "block";
 }
 
+submitbtn.onclick = function() {
+    modal.style.display = "none";
+}
+
+
 // When the user clicks on <span> (x), close the modal
 span.onclick = function() {
     modal.style.display = "none";
 }
+
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
@@ -106,3 +113,5 @@ window.onclick = function(event) {
         modal.style.display = "none";
     }
 }
+
+
